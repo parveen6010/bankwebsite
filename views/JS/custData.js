774622dc2b70448c53c7c4b81d3e8571e9377081
Parveen Prajapati <parveenprajapati6010@gmail.com>
2022-05-18 03:21:34 +0530

    int

---
+++ views/JS/custData.js
@@ -0,0 +1,51 @@
+let custData = [{
+    "account_no": "3576305161",
+    "name": "parveen prajapati",
+    "email": "parveen999@gmail.com",
+    "balance": "10757.00"
+  }, {
+    "account_no": "2845051928",
+    "name": "Spark ",
+    "email": "spark@pinterest.com",
+    "balance": "1311.54"
+  }, {
+    "account_no": "5824674190",
+    "name": "Devesh",
+    "email": "devesh@histats.com",
+    "balance": "23224.79"
+  }, {
+    "account_no": "1394614362",
+    "name": "Parveen",
+    "email": "parveen77@hotmail.com",
+    "balance": "8587.86"
+  }, {
+    "account_no": "3026504384",
+    "name": "Nikhil ",
+    "email": "nikhil@yahoo.com",
+    "balance": "6438.80"
+  }, {
+    "account_no": "3656457441",
+    "name": " Gawai",
+    "email": "gawai@outlook.net",
+    "balance": "1052.84"
+  }, {
+    "account_no": "4357994632",
+    "name": "Vishal pandey",
+    "email": "vishal@gmail.com",
+    "balance": "9952.50"
+  }, {
+    "account_no": "0715122916",
+    "name": "Akash Bhagne",
+    "email": "akash@live.in",
+    "balance": "9087.60"
+  }, {
+    "account_no": "5611890172",
+    "name": "Rahul Singh",
+    "email": "Rahul@gmail.com",
+    "balance": "2251.32"
+  }, {
+    "account_no": "4854295384",
+    "name": "Samir",
+    "email": "samir@live.com",
+    "balance": "5680.26"
+  }];
